675928203fd0baa76b474ac77102743cc11f4499
Raghav Rungta <93864399+loopgoeson@users.noreply.github.com>
2023-10-26 01:04:54 +0530

    adding data of medicines

---
+++ src/Data/Medicines.js
@@ -0,0 +1,32 @@
+import dolo from '../Asset/dolo.jpg'
+import saridon from '../Asset/saridon.jpg'
+import azithromycin from '../Asset/azithromycin.jpg'
+import citrizine from '../Asset/citrizine.jpg'
+
+let products =[
+{
+    id:1,
+    name:'Dolo',
+    price:200,
+    image:dolo,
+},
+{
+    id:2,
+    name:'Saridon',
+    price:350,
+    image:saridon
+},
+{
+    id:3,
+    name:'Azithromycin',
+    price:400,
+    image:azithromycin
+},
+{
+    id:4,
+    name:'Citrizine',
+    price:600,
+    image:citrizine
+}
+];
+export default products;
