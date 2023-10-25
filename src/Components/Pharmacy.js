4b93809eb5ccac0a9fa7e18a54453dcb025f9842
Raghav Rungta <93864399+loopgoeson@users.noreply.github.com>
2023-10-26 01:05:27 +0530

    adding products to pharmacy page

--- src/Components/Pharmacy.js
+++ src/Components/Pharmacy.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import products from '../Data/Medicines'
+import Item from './Item'
+import './Pharmacy.css'
 
 const Pharmacy = () => {
   return (
-    <div>
-      pharmacy 
+    <div className='product'>
+      {products.map((item,i)=>{
+        return <Item key={i} id={item.id} name={item.name} image={item.image} price={item.price}/>
+      })}
     </div>
   )
 }
