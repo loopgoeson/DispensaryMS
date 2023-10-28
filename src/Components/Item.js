d853417a4ab474b91865ecf2a1e4bd833215c33f
abhayyy30 <abhayyy.30@gmail.com>
2023-10-28 23:29:53 +0530

    updating home

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Item = (props) => {
   return (
-<Card style={{ width: '18rem' }}>
-<Card.Img variant="top" src= {props.image}/>
-<Card.Body>
-  <Card.Title>{props.name}</Card.Title>
-  <Card.Text>
-    Price: ₹{props.price}
-  </Card.Text>
-  <Button variant="primary">Add to cart</Button>
-</Card.Body>
-</Card>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={props.image} />
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>Price: ₹{props.price}</Card.Text>
+        <Button variant="primary">Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
-  )
-}
-
-export default Item
+export default Item;
