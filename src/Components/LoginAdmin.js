043a6aa46742c4162af7891b3cbb20568eebffb9
Raghav Rungta <93864399+loopgoeson@users.noreply.github.com>
2023-10-30 01:14:11 +0530

    updated login page

--- src/Components/LoginAdmin.js
+++ src/Components/LoginAdmin.js
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Card } from 'react-bootstrap';
+import dr from "../Asset/dr1.png";
+import './LoginAdmin.css'
 const LoginAdmin = () => {
   return (
-    <div>
-      <p>login for Admin</p>
-    </div>
+    <Form className='form'>
+        <Card >
+          <Card.Img className='img' variant="top" src={dr} />
+          <Card.Body>
+            <Card.Title style={{fontWeight:700, marginLeft:160, fontSize:28}}>Admin</Card.Title>
+          </Card.Body>
+        </Card>
+    <Form.Group className="pwd mb-3 " controlId="formBasicPassword">
+      <Form.Control type="password" placeholder=" Enter Password" />
+    </Form.Group>
+    <Button variant="outline-primary" type="submit">
+      Submit
+    </Button>
+   
+  </Form>
   )
 }
 
--- src/Components/LoginDoctor.js
+++ src/Components/LoginDoctor.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import './LoginDoctor.css'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import React, { useState } from 'react';
 
-const LoginDoctor = () => {
+function LoginDoctor(){
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log('Appointment details submitted:', );
+  };
   return (
-    <div>
-      login for Doctor
-    </div>
+   <Form className='form' onSubmit={handleFormSubmit}>
+     <div>
+           <label>Doctor Name :</label>
+           <select
+            value={selectedOption}
+            onChange={(e)=> setSelectedOption(e.target.value)}
+           >
+            <option value="" disabled>
+              Select a Doctor
+            </option>
+            <option value="bhatra">Dr. Gambhir Bhatra </option>
+            <option value="sinha">Dr. Manoj Sinha </option>
+            <option value="rao">Dr. Gaurav Rao</option>
+           </select>
+        </div>
+       <div>
+         
+       </div>
+       <div>
+       <Button variant="outline-primary" type="submit">
+      Submit
+    </Button>
+       </div>
+   
+   </Form>
   )
 }
+
 
 export default LoginDoctor
