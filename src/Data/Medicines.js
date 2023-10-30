0e58f7fdd6c7c7e8d0a9c1369b3938be88664169
prathamdasani <100029204+prathamdasani@users.noreply.github.com>
2023-10-31 02:55:44 +0530

    medicines

--- src/Data/Medicines.js
+++ src/Data/Medicines.js
@@ -3,6 +3,7 @@ import saridon from '../Asset/saridon.jpg'
 import azithromycin from '../Asset/azithromycin.jpg'
 import citrizine from '../Asset/citrizine.jpg'
 
+
 let products =[
 {
     id:1,
@@ -14,19 +15,140 @@ let products =[
     id:2,
     name:'Saridon',
     price:350,
-    image:saridon
+    image:saridon,
 },
 {
     id:3,
     name:'Azithromycin',
     price:400,
-    image:azithromycin
+    image:azithromycin,
 },
 {
     id:4,
     name:'Citrizine',
     price:600,
-    image:citrizine
+    image:citrizine,
 },
+{
+    id:5,
+    name:'Dilminox',
+    price:150,
+    image:,
+},
+{
+    id:6,
+    name:'Finarise 5mg',
+    price:125,
+    image:,
+},
+{
+    id:7,
+    name:'Azithrol 500',
+    price:110,
+    image:,
+},
+{
+    id:8,
+    name:'Sinarest LP',
+    price:105,
+    image:,
+},
+{
+    id:9,
+    name:'Omefol',
+    price:340,
+    image:,
+},
+{
+    id:10,
+    name:'Dytor 10',
+    price:75,
+    image:,
+},
+{
+    id:11,
+    name:'Glimepiride 0.5',
+    price:60,
+    image:,
+},
+{
+    id:12,
+    name:'Bisonext 10',
+    price:200,
+    image:,
+},
+{
+    id:13,
+    name:'Ramitorva',
+    price:45,
+    image:,
+},
+{
+    id:14,
+    name:'Uprise D3',
+    price:225,
+    image:,
+},
+{
+    id:15,
+    name:'Oflox 200',
+    price:90,
+    image:,
+},
+{
+    id:16,
+    name:'Drotikind M',
+    price:85,
+    image:,
+},
+{
+    id:17,
+    name:'Microtel 40',
+    price:60,
+    image:,
+},
+{
+    id:18,
+    name:'Flora BC',
+    price:40,
+    image:,
+},
+{
+    id:19,
+    name:'Desval ER 500',
+    price:145,
+    image:,
+},
+{
+    id:20,
+    name:'Zenoxa 150',
+    price:75,
+    image:,
+},
+{
+    id:21,
+    name:'Acutret 10',
+    price:210,
+    image:,
+},
+{
+    id:22,
+    name:'Zeptol 10',
+    price:20,
+    image:,
+},
+{
+    id:23,
+    name:'Zerodol',
+    price:55,
+    image:,
+},
+{
+    id:24,
+    name:'Nuroday',
+    price:215,
+    image:,
+}, 
+
 ];
 export default products;
