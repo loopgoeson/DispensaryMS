8b7abd57419f6715296733487b38878fa90f7d1a
Raghav Rungta <93864399+loopgoeson@users.noreply.github.com>
2023-11-01 23:51:31 +0530

    adding hover effect to items

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import './item.css'
 
 const Item = (props) => {
   return (
-    <Card style={{ width: '18rem', display:'flex', paddingLeft:20}}>
+    <Card className="item" style={{ width: '18rem', display:'flex', paddingLeft:20}}>
     <Card.Img style={{ width:350, height:250}} variant="top" src= {props.image} alt="Product-image not available"/>
     <Card.Body>
       <Card.Title style={{ fontWeight:700, fontSize:22}}>{props.name}</Card.Title>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
     <nav>
         <ul className='text-xl flex space-x-6 justify-center py-10'>
             <NavLink to="/" >Home </NavLink>
-            <NavLink to="/Patient" >Patient</NavLink>
+            
             <NavLink to="/About" >About</NavLink>
             <NavLink to="/Contact" >Contact Us</NavLink>
         </ul>
