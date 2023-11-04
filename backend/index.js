441bb185acdfe2192ce860ff2217dd488b01a9f3
Raghav Rungta <93864399+loopgoeson@users.noreply.github.com>
2023-11-04 21:43:36 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
-const PORT=3001;
+const cors=require("cors");
 
 const app= express();
+const PORT=3001;
 
+app.use(cors());
+
+app.use(express.json);
+app.use(express.urlencoded({extended:false}));
+
+ app.get('')
 app.listen(PORT , ()=>{
     console.log(`Server is running on port ${PORT}`);
 });
